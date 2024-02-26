d68f79c1741dfe6cbd8db3ed68343d3c6ad655c2
ShirDaraby <156781517+ShirDaraby@users.noreply.github.com>
2024-02-26 18:49:50 +0200

    ADD website_airproof

---
+++ website_airproof/__manifest__.py
@@ -0,0 +1,16 @@
+{
+   'name': 'Airproof Theme',
+   'description': 'Airproof Theme',
+   'category': 'Theme/Creative',
+   'version': '1.0.0',
+   'depends': ['theme_common'],
+   'data': [
+      'data/ir_asset.xml',
+   ],
+    'assets': {
+    'web._assets_primary_variables': [
+    #   ('prepend', 'website_airproof/static/src/scss/primary_variables.scss'),
+      'website_airproof/static/src/scss/fonts.scss'
+   ],
+},
+}

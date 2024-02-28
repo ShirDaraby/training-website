f6ad825d3ce8c46c52fa3403840a2fb4bf389f3c
hedshefer <hed@laylinetech.com>
2024-02-28 10:33:49 +0200

    WIP

--- website_airproof/__manifest__.py
+++ website_airproof/__manifest__.py
@@ -3,13 +3,13 @@
    'description': 'Airproof Theme',
    'category': 'Theme/Creative',
    'version': '1.0.0',
-   'depends': ['theme_common'],
+   'depends': ['website'],
    'data': [
-      'data/ir_asset.xml',
+      # 'data/ir_asset.xml',
    ],
     'assets': {
     'web._assets_primary_variables': [
-      # ('prepend', 'website_airproof/static/src/scss/primary_variables.scss'),
+      'website_airproof/static/src/scss/primary_variables.scss',
       'website_airproof/static/src/scss/fonts.scss'
    ],
 },

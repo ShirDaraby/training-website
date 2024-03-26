d15c629c6a90e36694aef43ad6f3d1731fc192c3
CVDE-odoo <cvde@odoo.com>
2024-03-26 14:46:14 +0100

    [ADD] feedback
    A few comments :)

--- website_airproof/__manifest__.py
+++ website_airproof/__manifest__.py
@@ -1,8 +1,15 @@
 {
    'name': 'Airproof Theme',
    'description': 'Airproof Theme',
+   # add the version of Odoo
    'category': 'Theme/Hidden',
+   # dependencies: add 'website_sale' and 'website_sale_wishlist' ('website' will be installed by default as a dependency)
    'depends': ['website'],
+   # add the licence : OEEL-1 (Odoo Enterprise Edition License v1.0)
+   # I think you need to add your thumbnail here: to test
+   #'images': [
+        #'static/description/clean_screenshot.jpg',
+    #],
    'data': [
       # 'data/ir_asset.xml',
       'data/menu.xml',

c8e11f87349d8089a71112aa1e367cbe6e6fcb4c
Shir Daraby <shir@laylinetech.com>
2024-06-27 13:23:47 +0300

    two sections on homepage

--- website_bootstrap/__manifest__.py
+++ website_bootstrap/__manifest__.py
@@ -4,17 +4,21 @@
    # add the version of Odoo
    'category': 'website/theme',
    # dependencies: add 'website_sale' and 'website_sale_wishlist' ('website' will be installed by default as a dependency)
-   'depends': ['website','website_sale', 'website_sale_wishlist'],
+   'depends': ['website','website_sale','website_sale_wishlist'],
    # add the licence : OEEL-1 (Odoo Enterprise Edition License v1.0)
    # I think you need to add your thumbnail here: to test
    'images': [
         'static/description/clean_screenshot.jpg',
     ],
     'license': 'OEEL-1',
+   
    'data': [
       'data/menu.xml',
       'data/presets.xml',
+      'data/images.xml',
       'views/website_template.xml', 
+      # pages
+      'data/pages/home.xml',
    ],
 
     'assets': {
